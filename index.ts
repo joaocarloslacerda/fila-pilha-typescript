87c1ac78719bb66ae1847b994a3ebc9040be29e3
joao <juniorlacerdasilva@gmail.com>
2024-06-02 16:17:46 -0300

    criando class

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("teste")
+interface Iitems {
+    item: string[]
+}
+
+class List implements Iitems {
+    item: string[]
+
+    // private item = []
+    constructor(item: string[]){
+        this.item = item;
+    }
+}
